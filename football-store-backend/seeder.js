0318bf7aa6c55dd5f82afb2bc9253e3b800a0506
Fredric Törnkvist <fredricff@hotmail.com>
2026-05-28 21:48:30 +0200

    Updated seeder.js

--- football-store-backend/seeder.js
+++ football-store-backend/seeder.js
@@ -1,13 +1,38 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Product = require("./models/productModel");
+
+//Importera data från de olika ligorna
 const allsvenskanData = require("./data/allsvenskan");
 const serieAData = require("./data/seriea");
 const laLigaData = require("./data/laliga");
 const premierLeagueData = require("./data/premierleague");
 
-const allProducts = [
-  ...allsvenskanData,
-  ...serieAData,
-  ...laLigaData,
-  ...premierLeagueData,
-];
+// Anslut till MongoDB
+mongoose.connect(process.env.CONNECTION_STRING);
 
-await Product.Product(allProducts);
+const importData = async () => {
+  try {
+    // Rensa befintliga produkter
+    await Product.deleteMany();
+
+    // Slå ihop alla produkter från smaliga ligorna till en array
+
+    const allProducts = [
+      ...allsvenskanData,
+      ...serieAData,
+      ...laLigaData,
+      ...premierLeagueData,
+    ];
+
+    await Product.insertMany(allProducts);
+
+    console.log("✅ Datan är seedad och klar!");
+    process.exit(); // Stäng skriptet
+  } catch (error) {
+    console.error(`❌ Fel vid seedning: ${error.message}`);
+    process.exit(1); // Stäng skriptet med en felkod
+  }
+};
+
+importData();
